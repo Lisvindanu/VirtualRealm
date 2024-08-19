cd72b8e779463de05ce9f8b6bb9bae55454a90fb
Lisvindanu <Lisvindanu015@gmail.com>
2024-08-19 08:04:59 +0700

    Fix Security Auth User

--- src/main/kotlin/com/virtualrealm/our/gameMarketPlaces/controller/ErrorController.kt
+++ src/main/kotlin/com/virtualrealm/our/gameMarketPlaces/controller/ErrorController.kt
@@ -12,10 +12,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class ErrorController {
     @ExceptionHandler(value = [ConstraintViolationException::class])
     fun validationHandler(constraintViolationException: ConstraintViolationException): WebResponse<String> {
+        val messages = constraintViolationException.constraintViolations.joinToString(", ") { it.message ?: "Unknown error" }
         return WebResponse(
             code = 400,
             status = "Bad Request",
-            data = constraintViolationException.message!!
+            data = messages
         )
     }
 
@@ -31,9 +32,17 @@ class ErrorController {
     @ExceptionHandler(value = [UnAuthorizedException::class])
     fun  unauthorized(notFoundException: UnAuthorizedException): WebResponse<String> {
         return WebResponse(
-            code = 404,
+            code = 401,
             status = "UNAUTHORIZED",
             data = "Please Put Your Api Keys"
+        )
+    }
+    @ExceptionHandler(value = [Exception::class])
+    fun handleGenericException(exception: Exception): WebResponse<String> {
+        return WebResponse(
+            code = 500,
+            status = "Internal Server Error",
+            data = exception.message ?: "An unexpected error occurred."
         )
     }
 
