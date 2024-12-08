525bca7e47b95187308d4b30faef27e52195ab7b
Lisvindanu <Lisvindanu015@gmail.com>
2024-12-08 22:28:44 +0700

    update

--- src/main/kotlin/com/virtualrealm/our/gameMarketPlaces/repository/ApiKeyRepository.kt
+++ src/main/kotlin/com/virtualrealm/our/gameMarketPlaces/repository/ApiKeyRepository.kt
@@ -3,5 +3,6 @@ package com.virtualrealm.our.gameMarketPlaces.repository
 import com.virtualrealm.our.gameMarketPlaces.entity.ApiKey
 import org.springframework.data.jpa.repository.JpaRepository
 
+@EnableJpaRepositories
 interface ApiKeyRepository: JpaRepository<ApiKey, String> {
 }
